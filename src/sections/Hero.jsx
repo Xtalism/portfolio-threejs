487f65006a54a48aa9d2b03cd0e430ae53e81dba
Xtalism <manuel_lego1234@hotmail.com>
2025-04-21 15:18:25 -0600

    Hero Section:= 'Changed robot animation'

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
     <section className='min-h-screen border-blue-500 w-full  flex flex-col relative' id="home">
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
             <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>
-                Hi, I'm Manuel <span className='waving-hand'>👋</span>
+                Hi, I'm Manuel <span className='waving-robot'>🤖</span>
             </p>
             <p className='hero_tag text-gray_gradient'>
                 Finding Solutions to any Problem.
